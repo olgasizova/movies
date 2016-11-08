e9825659632ed4965edf2c3e764950fe2ac3116e
Olga Sizova <olisizova@gmail.com>
2016-11-08 12:48:40 -0500

    updated movie.js file

--- models/movie.js
+++ models/movie.js
@@ -1,6 +1,66 @@
 const db = require('../lib/dbConnect');
 
+//get all movies
+module.exports = {
+
+function getAllMovies(req, res, next) {
+    db.any('SELECT * FROM movies;')
+      .then((movies) => {
+        res.rows = movies;
+        next();
+      })
+      .catch(error => next(error));
+  },
+
+// get movie
+module.exports = {
+
+function getMovie(req, res, next) {
+    db.one('SELECT * FROM movies;')
+      .then((movies) => {
+        res.rows = movies;
+        next();
+      })
+      .catch(error => next(error));
+  },
+
+// update movie
+module.exports = {
+
+function updateMovies(req, res, next) {
+    db.any('SELECT * FROM movies;')
+      .then((movies) => {
+        res.rows = movies;
+        next();
+      })
+      .catch(error => next(error));
+  },
+
+// delete movie
+  module.exports = {
+
+ function deleteMovie(req, res, next) {
+    db.any('SELECT * FROM movies;')
+      .then((movies) => {
+        res.rows = movies;
+        next();
+      })
+      .catch(error => next(error));
+  },
+
+  // get all movies with ratings (bonus)
+  module.exports = {
+
+ function getAllMoviesWithRatings(req, res, next) {
+    db.any('SELECT * FROM movies;')
+      .then((movies) => {
+        res.rows = movies;
+        next();
+      })
+      .catch(error => next(error));
+  },
 // Your middleware MUST allow limit and offset to be sent
+
 // via query parameters to the db for filtering
 
 // default limit
@@ -8,26 +68,6 @@ const limit = 10;
 // default offset
 const offset = 0;
 
-function getAllMovies(req, res, next) {
-// implement get all movies
-}
-
-function getMovie(req, res, next) {
-// implement get single movie
-}
-
-function updateMovie(req, res, next) {
-// implement update
-}
-
-function deletemovie(req, res, next) {
-// implement delete
-}
-
-// BONUS
-function getAllMoviesWithRatings(req, res, next) {
-
-}
 
 module.exports = {
   getAllMovies,
